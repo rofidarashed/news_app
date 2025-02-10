b2607e451c603bf55d453e461f7704b72d3e2b84
rofidarashed <rofidaelmaghraby@gmail.com>
2025-02-11 00:37:07 +0200

    dio

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -1,10 +1,10 @@
 class NewsModel {
   final String image;
   final String title;
-  final String subtitle;
+  final String description;
   const NewsModel({
     required this.image,
     required this.title,
-    required this.subtitle,
+    required this.description,
   });
 }
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,7 +1,26 @@
 import 'package:dio/dio.dart';
+import 'package:news_app/models/news_model.dart';
 
 class NewsService {
   final Dio dio;
 
   NewsService({required this.dio});
+
+  getTopNews() async {
+    Response response = await dio.get(
+        'https://newsdata.io/api/1/latest?apikey=pub_68179b829d0e7d17a102f0bc53db4e7c1929a&category=top&country=eg');
+
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> results = jsonData['results'];
+    List<NewsModel> articls = [];
+
+    for (var article in results)  {
+      NewsModel newsModel =  NewsModel(
+          image: article['image_url'],
+          title: article['title'],
+          description: article['description'],);
+      articls.add(newsModel);
+    }
+    print(articls);
+  }
 }
