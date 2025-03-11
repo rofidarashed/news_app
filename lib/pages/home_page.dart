2e26e0faa2aa01171f8fcc1ff5c15ba3f0fef983
rofidarashed <rofidaelmaghraby@gmail.com>
2025-03-11 17:34:12 +0200

    extract news list method

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/category_list.dart';
-import 'package:news_app/widgets/news_list.dart';
+import 'package:news_app/widgets/news_list_builder.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,20 +18,22 @@ class HomePage extends StatelessWidget {
             ),
             Text(
               'Cloud',
-              style: TextStyle(color: Colors.amber),
+              style: TextStyle(
+                color: Colors.amber,
+              ),
             ),
           ],
         ),
         centerTitle: true,
       ),
       body: const Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 8.0),
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
         child: CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
               child: CategoryList(),
             ),
-            NewsList(),
+            NewsListBuilder(),
           ],
         ),
       ),
